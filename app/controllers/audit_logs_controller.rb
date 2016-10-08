06852392567b040885675be5e93e61fb281137d3
LightbulbProductions <gerbermichelle23@gmail.com>
2016-10-08 08:20:40 -0400

    implemented  functional buttons for employees, no errors on tests

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -2,6 +2,13 @@ class AuditLogsController < ApplicationController
   def index
     @audit_logs = AuditLog.page(params[:page]).per(20)
     authorize @audit_logs
+  end
+  
+  def confirm
+    audit_log = AuditLog.find(params[:id])
+    authorize audit_log
+    audit_log.confirmed!
+    redirect_to root_path, notice: "Thank you! Confirmation is successful."
     #above must be in this order to instantiate @audit_logs
     #otherwise it is nil
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,16 +18,17 @@ puts "1 Admin User created"
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content  I must have had a longish sleep, for, when 
-I woke, the stars were shining down on my face. Sounds of the countryside came 
-faintly in, and the cool night air, veined with smells' of earth and salt, fanned my 
-cheeks. The marvelous peace of the sleepbound summer night flooded through me 
-like a tide.", user_id: @user.id, overtime_request: 2.5)
+  I woke, the stars were shining down on my face. Sounds of the countryside came 
+  faintly in, and the cool night air, veined with smells' of earth and salt, fanned my 
+  cheeks. The marvelous peace of the sleepbound summer night flooded through me 
+  like a tide.", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
 
-100.times do |audit_log|
-  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 2.days))
-end
 
-puts "100 audit logs have been created"
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 29.days))
+
+puts "3 audit logs have been created"
